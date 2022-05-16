6180dbe78eecbde4f32ce94ed4db605de185169d
Gaby <ggasubuyuj@scl.edu.gt>
2022-05-15 21:53:58 -0600

    Se agrego la programación a la ventana y correción de errores

--- js/Main.js
+++ js/Main.js
@@ -4,14 +4,31 @@ var menu = document.getElementById('menu');
 var ico_menu = document.getElementById('ico_menu');
 
 
+
+var ventana = document.getElementById('ventanita')
+var ico_explorer = document.getElementById('ico_explorer');
+
+
 /* FUNCION */
 
 function activar(){
     menu.classList.add("activar");
+    
+    
+}
+
+
+function ventanaActivate(){
+    ventana.classList.add("ventanaActivate");
 }
 
 
 /* EVENTO ALGO QUE PASA */
 
 ico_menu.addEventListener('click',activar);
+
+
+ico_explorer.addEventListener('click',ventanaActivate);
+
+
 
