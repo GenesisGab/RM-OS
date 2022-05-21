25d40ca24641f289455bd91e621e9b9cc2c3a981
Gaby <ggasubuyuj@scl.edu.gt>
2022-05-21 16:41:47 -0600

    Se agrego la programacion al icono del menu y de la ventana

--- js/Main.js
+++ js/Main.js
@@ -1,32 +1,102 @@
-/* ETIQUETAS DEL INDEX */
+/**********************************************************************/
+/* CREACION DE LA ETIQUETA ACTIVAR Y DESACTIVAR MENU */
+var activarMenu = document.getElementById("ico_menu");
 
-var menu = document.getElementById('menu');
-var ico_menu = document.getElementById('ico_menu');
+//FUNCION ACTIVAR Y DESACTIVAR MENU
+function activateMenu(){
+
+    var Menu = document.getElementById("menu");
+
+    if (Menu.style.display === "none"){
+        Menu.style.display = "inline-block";
+    }else{
+        Menu.style.display = "none";
+    }
+
+}
+//EVENTO PARA EL MENU
+activarMenu.addEventListener('click',activateMenu);
+/**********************************************************************/
 
 
-var ventana = document.getElementById('ventanita')
-var ico_explorer = document.getElementById('ico_explorer');
 
+/**********************************************************************/
+/* CREACION DE ETIQUETAS PARA ACTIVAR Y DESACTIVAR LA  VENTANA */
 
-/* FUNCION */
+var activarse = document.getElementById("ico_explorer");
 
-function activar(){
-    menu.classList.add("activar");
+//FUNCIÓN PARA ABRIR Y CERRAR LA VENTANA
+function ActivarDesactivar() {    
+        
+    var explorer = document.getElementById("ventanita");
+    
+    console.log("valor x",explorer);
+
+    //Activa y desactiva la ventana
+    if (explorer.style.display === "none") {
+        explorer.style.display = "block";
+        
+    } else {
+        explorer.style.display = "none";
+    }
+
 }
 
+/* EVENTO */
+activarse.addEventListener('click', ActivarDesactivar);
+/**********************************************************************/
 
-function ventanaActivate(){
-    ventana.classList.add("ventanaActivate");
+
+
+/******************************************************************** */
+/* CREACION DE LA VARIABLE PARA AMPLIAR */
+var ampliar = document.getElementById("ampliar");
+
+/* FUNCION PARA MAXIMIZAR LA VENTANA */
+function maximizar(){
+    var maximizar = document.getElementById("ventanita");
+
+    console.log("maximizar ",maximizar);
+
+    if(maximizar.style.width === "100%"){
+        maximizar.style.width = "600px";
+        maximizar.style.height = "400px";
+    }else{
+        maximizar.style.width = "100%";
+        maximizar.style.height = "100%";
+       
+    }
 }
 
+/*EVENTO PARA AMPLIAR O MAXIMIZAR LA VENTANA */
+ampliar.addEventListener('click', maximizar);
+/******************************************************************** */
 
-/* EVENTO ALGO QUE PASA */
-
-ico_menu.addEventListener('click',activar);
 
 
-ico_explorer.addEventListener('click',ventanaActivate);
+/**********************************************************************/
+/* CREACION DE VARIABLES PARA EL BOTON CERRAR Y MINIMIZAR */
 
+var closeWindow = document.getElementById("cerrar");
+var minimizar = document.getElementById("minimizar");
+
+/* FUNCION PARA DESACTIVAR LA VENTANA */
+function cerrar(){
+
+    var cerrarExplorer = document.getElementById("ventanita");
+
+    console.log("cerrar,",cerrarExplorer);
+
+        if (cerrarExplorer.style.display === "block"){
+            cerrarExplorer.style.display = "none";
+        }
+
+    }
+
+    //EVENTOS
+    closeWindow.addEventListener('click',cerrar);
+    minimizar.addEventListener('click',cerrar);
+/**********************************************************************/
 
 
 
